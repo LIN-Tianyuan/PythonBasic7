5a9fa2f859a9d3fe2b311c62e5de385b0459b833
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2025-02-01 13:04:26 +0100

    week 11

---
+++ Chapter4/Course/03_continue.py
@@ -0,0 +1,8 @@
+i = 2
+while i < 11:
+    if i % 2 == 0:
+        if i == 8:
+            i = i + 1
+            continue
+        print(i)
+    i = i + 1
---
+++ Chapter4/Course/exercise3.py
@@ -0,0 +1,7 @@
+a = 0
+i = a
+while a < 100 and a % 2 == 0:
+    a = a + 2   # 2
+    i = i + a   # 2
+print(i)
+    
