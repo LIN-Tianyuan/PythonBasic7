3410a5ec8c314a3d5bd19d68f27ac5bc557ee71b
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2025-02-08 13:14:37 +0100

    week 13

---
+++ Chapter4/Course/05_for2.py
@@ -0,0 +1,12 @@
+"""
+for number in range(11):
+    print(number)
+"""
+
+"""
+for number in range(1, 11):
+    print(number)
+"""
+
+for number in range(1, 11, 2):
+    print(number)
---
+++ Chapter4/Course/exercise4.py
@@ -0,0 +1,2 @@
+for number in (1, 11, 1) and range(7, 77, 7):
+    print(number)
